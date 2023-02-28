67bda53c8ee35ac63ad35e513ae3740c9513952d
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-02-28 17:45:41 +0100

    working project, mobile first

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const menu = document.querySelector('#menu');
 const nav = document.querySelector('nav');
+const pIntro1 = document.querySelector('#pattern1');
+const pIntro2 = document.querySelector('#pattern2');
+const pHWW = document.querySelector('#pattern3');
+const pFooter = document.querySelector('#pattern4');
+
 menu.addEventListener('click',()=>{
     
     if(menu.getAttribute('src')== './images/icon-hamburger.svg'){
@@ -8,5 +13,24 @@ menu.addEventListener('click',()=>{
     }else{
         menu.setAttribute('src', './images/icon-hamburger.svg');
         nav.classList.add('vanish');
+    }
+})
+
+window.addEventListener('resize',()=>{
+    if(window.innerWidth > 768){
+        menu.style.display = 'none';
+        nav.classList.remove('vanish');
+        pIntro1.setAttribute('src', './images/bg-pattern-intro-left-desktop.svg')
+        pIntro2.setAttribute('src', './images/bg-pattern-intro-right-desktop.svg')
+        pHWW.setAttribute('src', './images/bg-pattern-how-we-work-desktop.svg')
+        pFooter.setAttribute('src', './images/bg-pattern-footer-desktop.svg')
+    }
+    if(window.innerWidth < 768){
+        menu.style.display = 'block';
+        nav.classList.add('vanish');
+        pIntro1.setAttribute('src', './images/bg-pattern-intro-left-mobile.svg')
+        pIntro2.setAttribute('src', './images/bg-pattern-intro-right-mobile.svg')
+        pHWW.setAttribute('src', './images/bg-pattern-how-we-work-mobile.svg')
+        pFooter.setAttribute('src', './images/bg-pattern-footer-mobile.svg')
     }
 })
